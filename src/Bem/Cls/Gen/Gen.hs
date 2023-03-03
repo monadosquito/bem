e50f79ace64aa19175b85cb9705b3cdaa889113b
monadosquito <monadosquito@gmail.com>
2023-03-03 10:59:34 +0300

    refactor(cls): share Ent decor and str
    The 'Ent' GADT with all its data constructors
    and the 'decor'
    and 'str' functions can be used
    from the 'Bem.Cls.Gen.Intr' module.

--- src/Bem/Cls/Gen/Gen.hs
+++ src/Bem/Cls/Gen/Gen.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 
 
@@ -8,83 +5,12 @@
 Generate a class of a combination of some entities from a scheme
 with the default decorations.
 -}
-module Bem.Cls.Gen.Gen
-           ( genBlk
-           , genElem
-           ) where
+module Bem.Cls.Gen.Gen where
 
 
+import Bem.Cls.Gen.Intr
 import Bem.Utl.Intr
 
-import Data.Char
-import Data.Kind
-import Data.List
-import Data.List.Split
-
-
-type FullStredElem = String
-
-type FullStredEnt = String
-
-type PrefixedStredEnt = String
-
-type StredEnt = String
-
-type StredEntPart = String
-
-
-data Ent (isMod :: Bool) where
-         Blk :: Show (b e m) => b (e :: Type -> Type) m -> Ent 'False
-         BoolMod :: Show m => Ent 'False -> m -> Ent 'True
-         Elem
-            :: (Show (b e bm), Show (e em), Show em)
-            => b e bm -> e em -> Ent 'False
-         Mod :: Show m => Ent 'False -> m -> Ent 'True
-
-
-adopt :: FromElem b FullStredElem
-adopt blk elem' = decoredBlk ++ "__" ++ decoredElem
-  where
-    decoredBlk = decor $ Blk blk
-    decoredElem = decor $ Elem blk elem'
-
-decor :: Ent isMod -> StredEnt
-decor ent
-    =
-    map toLower
-        $ case ent of
-              Blk blk
-                  ->
-                  concat . sep . part . trimFlds $ show blk
-              BoolMod _ boolMod
-                  ->
-                  concat . sep . part . trimFlds . unprefix $ show boolMod
-              Elem _ elem'
-                  ->
-                  concat . sep . part . unprefix $ show elem'
-              Mod prntEnt mod'
-                  | [modName, modVal] <- words . unprefix $ show mod'
-                  ->
-                  (concat . sep . part) modName
-                  ++
-                  "_"
-                  ++ (concat . sep . part) modVal
-                  | otherwise
-                  ->
-                  decor $ BoolMod prntEnt mod'
-  where
-    part :: StredEnt -> [StredEntPart]
-    part = tail . split (keepDelimsL $ whenElt isUpper)
-    sep :: [StredEntPart] -> [String]
-    sep = intersperse "-"
-    trimFlds :: String -> StredEnt
-    trimFlds = takeWhile (/= ' ')
-    unprefix :: PrefixedStredEnt -> StredEnt
-    unprefix prefixedStredEnt
-        | null unprefixedEnt = prefixedStredEnt
-        | otherwise = tail unprefixedEnt
-      where
-        unprefixedEnt = dropWhile (/= '_') prefixedStredEnt
 
 -- | Generate a class of a block and element with their modifiers.
 genBlk :: FromFullBlk b (FromFullElem b Class)
@@ -110,30 +36,3 @@ genElem prntBlk elem' elemMods
   where
     adoptedElem = str $ Elem prntBlk elem'
     decoredElemMods = map (str . Mod (Elem prntBlk elem')) elemMods
-
-str :: Ent isMod -> FullStredEnt
-str ent
-    =
-    case ent of
-        Elem blk elem' -> adopt blk elem'
-        blk@(Blk _) -> decor blk 
-        boolMod@(BoolMod prntEnt _)
-            ->
-            let
-                decoredBoolMod = decor boolMod
-                stredPrntEnt = str prntEnt
-            in
-            stredPrntEnt ++ "_" ++ decoredBoolMod
-        modEnt@(Mod prntEnt mod')
-            ->
-            case words $ show mod' of
-                [_, _]
-                    ->
-                    let
-                        decoredMod = decor modEnt
-                        stredPrntEnt = str prntEnt
-                    in
-                    stredPrntEnt ++ "_" ++ decoredMod
-                _
-                    ->
-                    str $ BoolMod prntEnt mod'
